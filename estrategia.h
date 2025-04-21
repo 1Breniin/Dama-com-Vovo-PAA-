dacf4d0c583c18b279f5530166aa1cd3d1322d0d
Breno Esteves <brenoesteves243@gmail.com>
2025-04-21 18:57:04 -0300

    Delete

--- estrategia.h
+++
@@ -1,9 +0,0 @@
-#ifndef ESTRATEGIA_H
-#define ESTRATEGIA_H
-
-#include "tabuleiro.h"
-
-int estrategia_forca_bruta(Tabuleiro *tab);
-int estrategia_otimizada(Tabuleiro *tab);
-
-#endif
--- tempo.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEMPO_H
-#define TEMPO_H
-
-#include <sys/time.h>
-#include <sys/resource.h>
-
-void imprimir_tempos(struct timeval start, struct timeval end, struct rusage usage_start, struct rusage usage_end);
-
-#endif
